5db9cf69102a3298a95761022dbb5975a5fd474c
Alex <alexrozh1988@gmail.com>
2021-11-15 12:41:34 +0300

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Alexander Rozhdestvenskiy on 15.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
